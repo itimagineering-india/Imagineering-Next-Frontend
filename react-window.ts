dab795c1caa5555a162f37be3e257620b5d5a731
itimagineering-india <it.imagineering@gmail.com>
2026-03-30 15:20:58 +0530

    Create react-window.ts

---
+++ react-window.ts
@@ -0,0 +1,17 @@
+declare module "react-window" {
+  export const List: any;
+  export const Grid: any;
+  export const FixedSizeList: any;
+  export const FixedSizeGrid: any;
+  export const VariableSizeList: any;
+  export const VariableSizeGrid: any;
+  export const areEqual: any;
+  export const shouldComponentUpdate: any;
+  export const getScrollbarSize: any;
+  export const useDynamicRowHeight: any;
+  export const useGridCallbackRef: any;
+  export const useGridRef: any;
+  export const useListCallbackRef: any;
+  export const useListRef: any;
+}
+
